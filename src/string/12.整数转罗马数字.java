245db4ee9b4da0a86050d290ecf40d83522531fa
liuwei <wei.liu@shmedo.cn>
2020-06-05 16:44:26 +0800

    cmt

---
+++ src/string/12.整数转罗马数字.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=12 lang=java
+ *
+ * [12] 整数转罗马数字
+ */
+
+// @lc code=start
+class Solution {
+    public String intToRoman(int num) {
+        int[] nums = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] romans = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int index = 0;
+        StringBuilder sb = new StringBuilder();
+        while (index < 13) {
+            while (num >= nums[index]) {
+                sb.append(romans[index]);
+                num -= nums[index];
+            }
+            index++;
+        }
+        return sb.toString();
+    }
+}
+// @lc code=end
+
